ae9131510691adf24a4723298fe1ecd2a74f93b5
Joaozinhol <143085596+Joaozinhol@users.noreply.github.com>
2023-11-17 20:25:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,40 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
-document.querySelector('.tecla_pom').onclick = tocaSomPom;}
+function tocaSom (idElementoAudio){
+      const elemento = document.querySelector(idElementoAudio);   
+      if(elemento === null) {
+        alert('Elemento não encontrado');
+      }
+      if (elemento != null && elemento.localName === 'audio'){
+          elemento.play();
+      }else {
+         alert('Elemento não encontrado');
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-document.querySelector('.tecla_clap').onclick = tocaSomClap;}
+}}
+ const listaDeTeclas = document.querySelectorAll('.tecla');
+ //Estrutura de repetição - Enquanto 
+ //para que as teclas não sejam repetidas de maneira infinita, criamos o for, com o valor de início 0, e valor final chamado de contador < listaDeTeclas.lenght, para que a repetição seja exatamente do tamanho da nossa lista, que é de 9 teclas
+ for(let contador =0;contador < listaDeTeclas.length;contador++){
+    const tecla =listaDeTeclas[contador];
+    //classList armazena todas as informações do elemento pesquisado
+    const instrumento = tecla.classList[1];
+    // o idAudio é responsável por identificar o som do botão pesquisado 
+    const idAudio = `#som_${instrumento}`;
 
-function tocaSomTim(){
-    document.querySelector('#som_tecla_tim').play();
-document.querySelector('.tecla_tim').onclick = tocaSomTim;}
+    console.log(idAudio);
+    //onclick significa "ao clicar na tecla" a função toca som Será executada no ID daquele áudio será buscado e reproduzir
 
-function tocaSomPuff(){
-    document.querySelector('#som_tecla_puff').play();
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;}
-
-function tocaSomSplash(){
-    document.querySelector('#som_tecla_splash').play();
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;}
-
-function tocaSomToim(){
-    document.querySelector('#som_tecla_toim').play();
-document.querySelector('.tecla_toim').onclick = tocaSomToim;}
-
-function tocaSomPsh(){
-    document.querySelector('#som_tecla_psh').play();
-document.querySelector('.tecla_psh').onclick = tocaSomPsh;}
-
-function tocaSomTic(){
-    document.querySelector('#som_tecla_tic').play();
-document.querySelector('.tecla_tic').onclick = tocaSomTic;}
-
-function tocaSomTom(){
-    document.querySelector('#som_tecla_tom').play();
-document.querySelector('.tecla_tom').onclick = tocaSomTom;}
-
-
-
-
-
-
-
+    tecla.onclick = function(){
+       tocaSom(idAudio);
+    };
+    //onkeydown significa "quando a tecla estiver pressionada", nesse caso uma função será executada, e qual é essa função? é a estilização de mudança de cor do botão quando as tecla enter e space forem apertadas.
+    tecla.onkeydown = function(evento) {
+       if (evento.code ==='Enter'  || evento.code === 'Space'){
+       tecla.classList.add('ativa');
+    }
+};
+//onkeyup significa "quando a tecla estiver erguida, não pressionada nesse caso a função será removida
+    tecla.onkeyup = function() {
+       tecla.classList.remove('ativa');
+    };     
+    console.log(instrumento);
+console.log(contador);
+}
